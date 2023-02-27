124d4c427af173f495c5dd5c4118ac981679889e
praveenganeshcp <praveenganesh7@gmail.com>
2023-02-27 20:31:22 +0530

    add process id

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/ping', (req, res) => {
+    console.log('processing request by pid : ', process.pid);
     res.json({ message: "Hello world" });
 })
 
